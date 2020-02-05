95505b721a9aa8ee9a50b339436bbdbb6602b1c6
Manisha Katiyar <35479145+17BTCS023@users.noreply.github.com>
2020-02-05 12:17:08 +0530

    Add files via upload

--- caesorCipher.c
+++ caesorCipher.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int main(){
 
 char message[100],ch;
@@ -8,26 +9,40 @@ printf("Enter a message to encrypt: ");
 gets(message);
 printf("Enter key: ");
 scanf("%d", &key);
-for(i=0; message[i]!='\0';++i){
+
+for(i=0; message[i]!='\0';++i)
+{
 ch= message[i];
 if(ch>='a' && ch <='z'){
-ch=ch+key;
-if(ch>'z'){
-ch=ch-'z' + 'a'-1;
-}
-
-message[i]=ch;
+	ch=ch+key;
+	if(ch>'z') ch=ch-'z' + 'a'-1;
+	message[i]=ch;
 }
 
 else if(ch>='A' && ch <='Z'){
-ch=ch+key;
-if(ch>'Z'){
-ch=ch-'Z' + 'A'-1;
-}
-
-message[i]=ch;
-}
+	ch=ch+key;
+	if(ch>'Z') ch=ch-'Z' + 'A'-1;
+	message[i]=ch;
+	}
 }
 printf("Encrypted message is : %s \n", message);
+
+
+for(i=0; message[i]!='\0';++i){
+ch= message[i];
+if(ch>='a' && ch <='z'){
+	ch=ch-key;
+	if(ch>'z') ch=ch-'z' + 'a'-1;
+	message[i]=ch;
+}
+
+else if(ch>='A' && ch <='Z'){
+	ch=ch-key;
+	if(ch>'Z') ch=ch-'Z' + 'A'-1;
+	message[i]=ch;
+	}
+ }
+printf("Decrypted message is : %s \n", message);
+
 return 0;
 }
